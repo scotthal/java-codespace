0706d450ff332c00dcd621e63510aee9a66ce689
scotthal <scott@hallocks.net>
2023-08-17 12:38:17 -0600

    Add an environment setup script

---
+++ bin/env-java-space.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/opt/lang/java/openjdk
+export MAVEN_HOME=/opt/lang/java/maven
+export GRADLE_HOME=/opt/lang/java/gradle
+export PATH=$GRADLE_HOME/bin:$MAVEN_HOME/bin:$JAVA_HOME/bin:$PATH
